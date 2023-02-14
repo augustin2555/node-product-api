02f15888e19c9e0a0182a06beb8d4e21a868ee2c
augustin2555 <112929816+augustin2555@users.noreply.github.com>
2023-02-14 13:06:51 +0530

    Update dbConfig.js

--- dbConfig.js
+++ dbConfig.js
@@ -2,7 +2,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  "mongodb+srv://admin:MTY17ZehCeq6MUxY@cluster0.oyys1ne.mongodb.net/equp_rental?retryWrites=true&w=majority",
+  `mongodb+srv://${process.env.MONGO_USER_NAME}:${process.env.MONGO_USER_PASSWORD}@cluster0.oyys1ne.mongodb.net/equp_rental?retryWrites=true&w=majority`,
   { useNewUrlParser: true, useUnifiedTopology: true },
   (err) => {
     if (err) {
